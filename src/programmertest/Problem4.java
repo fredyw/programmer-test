9db5f2478c830444e7bd9851874980753cdfa4d2
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-05-09 15:14:38 -0500

    Fix a bug in problem 4
    I guess I'm not a software engineer after all :)

--- src/programmertest/Problem4.java
+++ src/programmertest/Problem4.java
@@ -10,17 +10,7 @@ public class Problem4 {
         List<String> strings = numbers.stream().map(e -> e.toString())
             .collect(Collectors.toList());
         Collections.sort(strings, (a, b) -> {
-            int minLength = Math.min(a.length(), b.length());
-            for (int i = 0; i < minLength; i++) {
-                int x = Integer.parseInt("" + a.charAt(i));
-                int y = Integer.parseInt("" + b.charAt(i));
-                if (x < y) {
-                    return 1;
-                } else if (x > y) {
-                    return -1;
-                }
-            }
-            return 0;
+            return Integer.valueOf(b + a).compareTo(Integer.valueOf(a + b));
         });
         StringBuilder sb = new StringBuilder();
         for (String s : strings) {
@@ -31,5 +21,7 @@ public class Problem4 {
     
     public static void main(String[] args) {
         System.out.println(largest(Arrays.asList(50, 2, 1, 9)));
+        System.out.println(largest(Arrays.asList(5, 50, 56)));
+        System.out.println(largest(Arrays.asList(420, 42, 423)));
     }
 }
